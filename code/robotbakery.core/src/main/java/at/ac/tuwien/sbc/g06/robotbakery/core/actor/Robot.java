39524ed15e9da7e67016101979e634f399862bbd
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2017-04-21 22:30:09 +0200

    Updates poms and sets up logger

--- code/robotbakery.core/src/main/java/at/ac/tuwien/sbc/g06/robotbakery/core/actor/Robot.java
+++
@@ -1,5 +0,0 @@
-package at.ac.tuwien.sbc.g06.robotbakery.core.actor;
-
-public abstract class Robot extends Actor {
-
-}
--- code/robotbakery.jsm/src/main/java/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-
-
-public class HelloWorld {
-// Dummy class so that directory structure gets commited
-}
